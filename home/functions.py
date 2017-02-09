30c731b4aefd71db31adc3b10a573b850f6b2121
Logan Patino <loganpatino10@gmail.com>
2017-02-09 14:16:42 -0500

    Add support for getting total score by mentioning 'Blart'. Also add definitions for future functionality.

---
+++ home/functions.py
@@ -0,0 +1,15 @@
+def get_current_standings():
+
+
+def get_total_score(name):
+    standings = requests.get('https://fantasy.premierleague.com/drf/leagues-classic-standings/508077').json()
+    score = 0
+    for player in standings['standings']['results']:
+        if player['player_name'] == name:
+            score = player['total']
+    return score
+
+def get_current_week_score(name):
+
+
+def get_current_position(name):
--- home/views.py
+++ home/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+import functions
 
-# Create your views here.
+@csrf_exempt
 def index(request):
+    if request.method == 'POST':
+        message_json = json.loads(request.body)
+        message_text = message_json['text']
+
+        if ('blart' in message_text.lower()) and (message_json['sender_type'] == 'user'):
+            player_name =  message_json['name']
+
+            for attachment in message_json['attachments']:
+                if attachment['type'] == 'mention':
+                    player_name = attachment['user_ids'][0]
+                    break
+            
+            score = functions.get_total_score(player_name)
+            return_message = 'Your current fantasy score is: {}'.format(score)
+            requests.post('https://api.groupme.com/v3/bots/post', data = {'bot_id': 'cb7fdea6c4bbcb0fea5ef5a5d8', 'text': return_message})
     return render(request, 'home/index.html')
