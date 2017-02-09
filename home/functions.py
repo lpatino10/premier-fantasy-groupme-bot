52550224f2a3094bc334dcc4550fd41813d30b2e
Logan Patino <loganpatino10@gmail.com>
2017-02-09 15:48:04 -0500

    Added support for McCoy's name because he had to be different

--- home/functions.py
+++ home/functions.py
@@ -1,8 +1,11 @@
 import requests
 
+username_dict = {'poonslayer': 'McCoy Patino'}
+
 #def get_current_standings():
 
 def get_total_score(name):
+    name = convert_name(name)
     standings = requests.get('https://fantasy.premierleague.com/drf/leagues-classic-standings/508077').json()
     score = 0
     for player in standings['standings']['results']:
@@ -13,3 +16,9 @@ def get_total_score(name):
 #def get_current_week_score(name):
 
 #def get_current_position(name):
+
+def convert_name(name):
+    new_name = name
+    if name in username_dict:
+        new_name = username_dict[name]
+    return new_name
