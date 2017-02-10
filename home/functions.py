62f6a27dac5cea2535823a36c2b2286541c8e75f
Logan Patino <loganpatino10@gmail.com>
2017-02-10 16:41:09 -0500

    Add NLC model to choose appropriate response

--- home/functions.py
+++ home/functions.py
@@ -1,25 +1,83 @@
 import requests
+from watson_developer_cloud import NaturalLanguageClassifierV1
 
+fantasy_url = 'https://fantasy.premierleague.com/drf/leagues-classic-standings/508077'
+base_url = 'https://api.groupme.com/v3'
+group_url = '/groups/'
+group_id = '17077713'
+
+classifier_id = 'f5b42fx173-nlc-3572'
+confidence_limit = 0.5
+
+not_yet_functioning_reponse = "I can't help you with this yet, but I should be able to soon!"
 username_dict = {'poonslayer': 'McCoy Patino',
   'Craig Casto': 'Craig C'}
 
-#def get_current_standings():
+def get_current_standings():
+    return not_yet_functioning_reponse
 
 def get_total_score(name):
     name = convert_name(name)
-    standings = requests.get('https://fantasy.premierleague.com/drf/leagues-classic-standings/508077').json()
+    standings = requests.get(fantasy_url).json()
     score = 0
     for player in standings['standings']['results']:
         if player['player_name'] == name:
             score = player['total']
     return score
 
-#def get_current_week_score(name):
+def get_current_week_score(name):
+    return not_yet_functioning_reponse
 
-#def get_current_position(name):
+def get_current_position(name):
+    return not_yet_functioning_reponse
 
 def convert_name(name):
     new_name = name
     if name in username_dict:
         new_name = username_dict[name]
     return new_name
+
+def get_mentioned_name(user_id):
+    name = ''
+    group_info = requests.get(base_url + group_url + group_id).json()
+    for member in group_info['members']:
+        if member['user_id'] == user_id:
+            name = member['nickname']
+    return convert_name(name)
+
+def get_message_subject(message_json):
+    player_name =  message_json['name']
+    for attachment in message_json['attachments']:
+        if attachment['type'] == 'mentions':
+            user_id = attachment['user_ids'][0]
+            player_name = get_mentioned_name(user_id)
+            break
+    player_name = convert_name(player_name)
+    return player_name
+
+def choose_response(message_json):
+    response = ''
+    message_text = message_json['text']
+
+    natural_language_classifier = NaturalLanguageClassifierV1(
+        username='6330ae18-60bc-4dfe-8c6d-3f86a7394b56',
+        password='MHlzYl0uGbHT')
+
+    classification_response = natural_language_classifier.classify(classifier_id, message_text)
+    top_class = classification_response['classes'][0]
+    if top_class['confidence'] < confidence_limit:
+        return response
+
+    name = get_message_subject(message_json)
+
+    top_class_name = top_class['class_name']
+    if top_class_name == 'standings':
+        response = get_current_standings()
+    elif top_class_name == 'total':
+        response = get_total_score(name)
+    elif top_class_name == 'week':
+        response = get_current_week_score(name)
+    elif top_class_name == 'position':
+        response = get_current_position(name)
+
+    return response
--- home/views.py
+++ home/views.py
@@ -1,25 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from home.functions import base_url
+from home.functions import choose_response
 from home.functions import get_total_score
 import json
 import requests
+
+post_url = '/bots/post'
+bot_id = 'cb7fdea6c4bbcb0fea5ef5a5d8'
 
 @csrf_exempt
 def index(request):
     if request.method == 'POST':
         message_json = json.loads(request.body)
-        message_text = message_json['text']
 
-        if ('blart' in message_text.lower()) and (message_json['sender_type'] == 'user'):
-            player_name =  message_json['name']
+        if message_json['sender_type'] == 'user':
 
-            for attachment in message_json['attachments']:
-                if attachment['type'] == 'mention':
-                    player_name = attachment['user_ids'][0]
-                    break
-            
-            score = get_total_score(player_name)
-            return_message = 'Your current fantasy score is: {}'.format(score)
-            requests.post('https://api.groupme.com/v3/bots/post', data = {'bot_id': 'cb7fdea6c4bbcb0fea5ef5a5d8', 'text': return_message})
+            # choose_response() will handle deciding the correct course of action and returning a proper response,
+            # which could be none at all
+            reply = choose_response(message_json)
+            if reply:
+                requests.post(base_url + post_url, data = {'bot_id': bot_id, 'text': reply})
+
     return render(request, 'home/index.html')
