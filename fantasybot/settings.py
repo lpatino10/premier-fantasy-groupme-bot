8f6b705ec9276817ee9ad009abfd1ec9f3d6447a
Logan Patino <loganpatino10@gmail.com>
2017-02-10 16:58:43 -0500

    Change logging settings

--- fantasybot/settings.py
+++ fantasybot/settings.py
@@ -129,3 +129,22 @@ STATICFILES_DIRS = (
 # https://warehouse.python.org/project/whitenoise/
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': '~/debug.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
